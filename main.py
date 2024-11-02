549e01e3033cfc346eee60be83409236e740a212
nWaldi <ppmip2@gmail.com>
2024-11-02 17:21:08 +0100

    new file, class and function which draw player

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         screen.fill(000000)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
