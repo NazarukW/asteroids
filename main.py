6b03fbf8b8e72847fb9b30d556e5ec001793c480
nWaldi <ppmip2@gmail.com>
2024-11-03 16:39:54 +0100

    player can move forward and backwards

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ def main():
 
     while True:
         screen.fill(000000)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
